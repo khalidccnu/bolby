a78eafbe6b4222a8aa65d963705b4b0585ac645d
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2022-11-20 17:13:39 +0600

    Add Layout

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -9,3 +9,64 @@ function navbar_toggle(collapseID) {
     if (!cID.classList.contains("collapse")) navbarTl.classList.replace("lni-grid-alt", "lni-close");
     else navbarTl.classList.replace("lni-close", "lni-grid-alt");
 }
+
+// ===== Header / Navlink Active =====
+let section = document.querySelectorAll("section");
+
+function navlink_active() {
+    let sY = scrollY;
+
+    section.forEach(function(item) {
+        let sectionTop = item.offsetTop - 250;
+        let sectionBottom = sectionTop + item.offsetHeight;
+        let sectionId = item.getAttribute("id");
+
+        if (sY > sectionTop && sY <= sectionBottom) document.querySelector(".navbar-nav a[href*=" + sectionId + "]").classList.add("active");
+        else document.querySelector(".navbar-nav a[href*=" + sectionId + "]").classList.remove("active");
+    });
+}
+
+// ===== Header / Fixed =====
+let header = document.querySelector("header");
+let link = document.querySelectorAll("a[href*='#']:not([href='#'])");
+
+link.forEach(function(item) {
+    item.addEventListener("click", function(event) {
+        if (innerWidth <= 767 && item.classList.contains("nav-link")) navbar_toggle("navbar-collapse");
+
+        let href = item.getAttribute("href");
+        let targetPos = document.querySelector(href).offsetTop;
+
+        if (innerWidth <= 767) {
+            scroll({
+                behavior: "smooth",
+                top: targetPos
+            });
+        } else {
+            scroll({
+                behavior: "smooth",
+                top: targetPos - header.offsetHeight
+            });
+        }
+
+        event.preventDefault();
+    });
+});
+
+// ===== Header / Sticky =====
+function header_sticky() {
+    if (innerWidth > 767) {
+        if (document.documentElement.scrollTop > 10) header.classList.add("sticky");
+        else header.classList.remove("sticky");
+    }
+}
+
+// ===== Initial Load =====
+navlink_active();
+header_sticky();
+
+// ===== Window Scroll Event =====
+onscroll = function() {
+    navlink_active();
+    header_sticky();
+}
